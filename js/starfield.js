fba95d285b1e02245999e93f33a989223215e6cd
Aline F. Brito <alinefbrito@gmail.com>
2021-05-10 19:17:01 -0300

    commit inicial
    Estrutura inicial do projeto do Jogo Space Invaders

---
+++ js/starfield.js
@@ -0,0 +1,16 @@
+class Starfield
+{
+	// define as variaveis
+	
+	fps = 30;
+    canvas = null;
+    width = 0;
+	height = 0;
+    minVelocity = 15;
+    maxVelocity = 30;
+    stars = 100;
+    intervalId = 0;
+	
+	
+	
+}
